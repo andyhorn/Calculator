3f887afdf9e83a06b1252c9d5f48ffd197d258e0
= <=>
2019-07-03 14:19:51 -0700

    Initial commit. Main file created and comment header added.

---
+++ Calculator/Main.cpp
@@ -0,0 +1,12 @@
+/*
+	Author:		Andrew Horn
+	Date:		6/3/2019
+	File:		Main.cpp
+	Overview:	Built to practice with C++ and challenge myself to make large calculations,
+				this is a console calculator app that will perform many operations that are
+				beyond the capabilities of a typical calculator.
+*/
+
+int main() {
+	return 0;
+}
